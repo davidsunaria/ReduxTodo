a5c15d00aa73816297c6105c71e7982609b0a0ba
david <er.daviddabwali@gmail.com>
2021-06-09 21:26:46 +0530

    first

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,16 @@ import TodoList from "./TodoList"
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
-let mystore = createStore(function (state = [{ title: "default todo" }], action) {
+// let mystore = createStore(function (state = [{ title: "default todo" }], action) {
 
-  switch (action.type) {
-    case "ADD":
-      let newState = [...state];
-      newState.push(action.payload)
-      return newState
-  }
-  return state
-})
+//   switch (action.type) {
+//     case "ADD":
+//       let newState = [...state];
+//       newState.push(action.payload)
+//       return newState
+//   }
+//   return state
+// })
 
 
 function App() {
